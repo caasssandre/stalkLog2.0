b0995352a4bb5ed51bfd8e5dbe4d1a9fa82f3f09
Cassandre-Guinut <guinutcassandre@gmail.com>
2019-12-31 10:45:40 +1300

    add profile component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
+import {connect} from 'react-redux'
+
 import Register from './Register'
 import Login from './Login'
 import Nav from './Nav'
+import Profile from './Profile'
 
-import {HashRouter as Router, Route, Link} from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom'
 
-const App = () => {
-  return (
-    <Router>
-      <Nav/>
-      <Route exact path='/login' component={Login}/>
-      <Route exact path='/register' component={Register}/>
-    </Router>
-  )
+class App extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <Router>
+        <Nav/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+        <Route exact path='/user' component={Profile}/>
+      </Router>
+    )
+  }
 }
 
-export default App
+function mapStateToProps(state){
+  return({
+    auth : state.auth
+  })
+}
+
+export default connect(mapStateToProps)(App)
